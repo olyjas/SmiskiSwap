f0b753006051fc9354cfdb6d1d7db9315976d2de
sthitp <sthitp@uw.edu>
2023-06-01 18:42:11 -0700

    sthitiaddpicpush

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ async function getDBConnection() {
 }
 
 //GET THE NAMES OF ALL OF THE SMISKI
-app.get('/allsmiskii', async function(req, res) {
+/*app.get('/allsmiskii', async function(req, res) {
   try {
     res.type('JSON');
     let db = await getDBConnection();
@@ -37,6 +37,38 @@ app.get('/allsmiskii', async function(req, res) {
     console.log(err);
     res.type('text').send('An error occurred on the server. Try again later.');
   }
+}); */
+
+
+//search and get the specfic smiskiis from a series if the search passed in was a series name
+app.get('/allsmiskiis', async function(req, res) {
+  try {
+    let search = req.query.search;
+    console.log("search");
+    console.log(search);
+    res.type('JSON');
+    let query;
+    let db = await getDBConnection();
+    let results = "";
+    console.log(1);
+    if (search) {
+      console.log(2);
+      query = 'SELECT names FROM smiskiinamesdata WHERE series = ?';
+      //search = '%' + search + '%';
+      console.log(search);
+      results = await db.all(query, search);
+    } else {
+      console.log("come here");
+      query = 'SELECT names FROM smiskiinamesdata';;
+      results = await db.all(query);
+      console.log(results);
+    }
+    db.close();
+    res.json(results);
+  } catch (err) {
+    res.status(500);
+    res.type('text').send('An error occurred on the server. Try again later.');
+  }
 });
 
 
@@ -46,6 +78,7 @@ app.get('/smiskiiseries', async function(req, res) {
     //let search = req.query.search;
     res.type('JSON');
     let db = await getDBConnection();
+    //use the distinct keyword instead
     let query = 'SELECT series FROM smiskiinamesdata';
     let results = await db.all(query);
     let finalResults = [];
@@ -67,34 +100,27 @@ app.get('/smiskiiseries', async function(req, res) {
 
 app.get('/series', async function(req, res) {
   try {
-    //let search = req.query.search;
     res.type('JSON');
     let db = await getDBConnection();
-    let query = 'SELECT series FROM smiskiinamesdata';
-    let results = await db.all(query);
-    let finalResults = [];
-    let index = 0;
-    for(let i = 0; i < results.length; i++) {
-      if(!(finalResults.includes(results[i].Series))) {
-        finalResults[index] = results[i].Series;
-        index++;
-      }
-    }
+    let seriesQuery = 'SELECT DISTINCT series FROM smiskiinamesdata';
+    let seriesResults = await db.all(seriesQuery);
+
     let finalFormat = [];
-    let secondIndex;
-    for(let j = 0; j < finalResults.length; j++) {
-      let secondQuery = 'SELECT names FROM smiskiinamesdata WHERE series=? ';
-      let names = await db.all(secondQuery, finalResults[j]);
-      finalFormat[secondIndex] = names;
-      secondIndex++;
-      //console.log(finalFormat[secondIndex]);
+
+    for (let i = 0; i < seriesResults.length; i++) {
+      let seriesName = seriesResults[i].Series;
+      let namesQuery = 'SELECT names FROM smiskiinamesdata WHERE series = ?';
+      let namesResults = await db.all(namesQuery, seriesName);
+      let names = namesResults.map(result => result.Names);
+
+      finalFormat.push({ series: seriesName, names: names });
     }
-    console.log("out");
+    //ARE WE ABLE TO ACCESS THE INDIVIDUAL NAMES ASSOCIATED WITH EACH SERIES
+    console.log("Formatted results:");
     console.log(finalFormat);
-    //let secondQuery = 'SELECT names from smiskiinamesdata';
-    //let names = await db.all(secondQuery);
+
     db.close();
-    res.json(finalResults);
+    res.json(finalFormat);
   } catch (err) {
     res.status(500);
     console.log(err);
--- public/index.js
+++ public/index.js
@@ -17,19 +17,145 @@
    */
   function init() {
 
+    /*----log in and sign up functionalities---*/
     /**
      * checks if the button exists and if it does, takes ths user to the
      * login and sign up page
      */
-    if (id("signup") !== null && id("login") !== null) {
-      id("signup").addEventListener("click", function() {
-        changePage("signup", "login.html");
-      });
+    // if (id("signup") !== null && id("login") !== null) {
+    //   id("signup").addEventListener("click", function() {
+    //     changePage("signup", "login.html");
+    //   });
 
-      id("login").addEventListener("click", function() {
-        changePage("login", "login.html");
-      });
+    //   id("login").addEventListener("click", function() {
+    //     changePage("login", "login.html");
+    //   });
+    //}
+
+    /*---log in and sign up functionalities end----*/
+
+    let search = id('search-term');
+
+    let searchBar = qs("#search-btn");
+    searchBar.addEventListener("click", function() {
+      startSearchSort(search.value);
+    });
+  }
+
+  function startSearchSort(search) {
+    fetchSeriesNames('/smiskiiseries')
+      .then((data) => {
+        console.log(data);
+        for(let i = 0; i < data.length; i++) {
+          if(search.toLowerCase() === data[i]){
+            console.log(1);
+            seriesSearchView(search);
+          }
+        }
+        // Use the fetched data here or perform additional operations
+      })
+      .catch(handleError);
+  }
+
+  function seriesSearchView(search) {
+    let url = "";
+    if (/\s/.test(search)) {
+      let allNames = data.name.split(" ");
+      for(let i = 0; i < allNames.length; i++) {
+           search += allNames.toLowerCase();
+      }
+      url = "/allsmiskiis/?search=" + search();
+    } else {
+      console.log(2);
+      url = "/allsmiskiis/?search=" + search.toLowerCase();
     }
+    fetchNamesinSeries(search, url);
+
+  }
+
+  // function fetchNamesinSeries(search, url) {
+  //   fetch(url)
+  //     .then(statusCheck)
+  //     .then((resp) => resp.json())
+  //     .then(buildSearchViewCards(search, data))
+  //     .catch(handleError);
+  // }
+  function fetchNamesinSeries(search, url) {
+    fetch(url)
+      .then(statusCheck)
+      .then((resp) => resp.json())
+      .then(async (data) => {
+        await buildSearchViewCards(search, data);
+        // Additional code here if needed
+      })
+      .catch(handleError);
+  }
+
+  function buildSearchViewCards(search, data) {
+    //this data should have all of the names
+    //data[i].Names
+    console.log(3);
+    for(let i = 0; i < 6; i++) {
+      let seriesArticle = gen('article');
+      seriesArticle.classList.add("series-card");
+      let smiskiiImg = gen('img');
+      console.log(data);
+      let names = data[i].Names;
+      if((/\s/.test(names)) || names.includes("-")) {
+        let allNames = "";
+        if(/\s/.test(names)) {
+          allNames = names.split(" ");
+        } else {
+          allNames = names.split("-");
+        }
+        names = "";
+        for(let i = 0; i < allNames.length; i++) {
+          console.log(allNames[i]);
+          names += allNames[i];
+        }
+        names = names.toLowerCase();
+      }
+      smiskiiImg.src = "/img/series/" + search + "/" + names + ".png";
+      console.log(smiskiiImg);
+      smiskiiImg.alt = search + " picture";
+      seriesArticle.appendChild(smiskiiImg);
+      let seriesName = gen('p');
+      seriesName.innerHTML = data[i].Names;
+      seriesName.classList.add("series-name-card");
+      seriesArticle.appendChild(seriesName);
+      seriesArticle.addEventListener("click", loadSmiskiiSearchView);
+      qs("#search-results").appendChild(seriesArticle);
+    }
+  }
+
+  function loadSmiskiiSearchView() {
+    console.log("hey");
+  }
+
+  function handleError(err) {
+    console.log(err);
+  }
+
+  /**
+   * gets all the yips for just one user
+   * @param {string} url - the endpoint for API
+   */
+  function fetchSeriesNames(url) {
+    return fetch(url)
+      .then(statusCheck)
+      .then((resp) => resp.json())
+      .catch(handleError);
+  }
+
+  /**
+   * checks that the data is valid and what the user was looking for
+   * @param {string} result - data fetched from api
+   */
+  async function statusCheck(result) {
+    if (!result.ok) {
+      throw new Error(await result.text());
+    }
+    return result;
   }
 
   /**
@@ -48,6 +174,24 @@
    */
   function id(id) {
     return document.getElementById(id);
+  }
+
+  /**
+   * Returns the element that has the qs attribute with the specified value.
+   * @param {string} selector - selector we are looking for.
+   * @returns {object} - DOM objects associated with selectors.
+   */
+  function qs(selector) {
+    return document.querySelector(selector);
+  }
+
+  /**
+   * Makes a new element and returns it
+   * @param {string} tagName - the element to be created.
+   * @returns {object} - the new element
+   */
+  function gen(tagName) {
+    return document.createElement(tagName);
   }
 
 })();
